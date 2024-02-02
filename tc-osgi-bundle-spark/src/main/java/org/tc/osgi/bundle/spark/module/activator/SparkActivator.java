7b0e77dd6addaca73e3cd82bd11dd2b31bfe76b9
Collonville Thomas <collonville.thomas@gmail.com>
2024-02-02 08:34:51 +0100

    fix: startup

--- tc-osgi-bundle-spark/src/main/java/org/tc/osgi/bundle/spark/module/activator/SparkActivator.java
+++ tc-osgi-bundle-spark/src/main/java/org/tc/osgi/bundle/spark/module/activator/SparkActivator.java
@@ -1,23 +1,17 @@
 package org.tc.osgi.bundle.spark.module.activator;
 
 import org.osgi.framework.BundleContext;
-import org.tc.osgi.bundle.groovy.interf.module.service.IGroovyService;
-
-import org.tc.osgi.bundle.spark.conf.SparkPropertyFile;
 import org.tc.osgi.bundle.spark.module.service.LoggerServiceProxy;
 import org.tc.osgi.bundle.spark.module.service.PropertyServiceProxy;
-import org.tc.osgi.bundle.spark.module.service.impl.SparkServiceImpl;
-import org.tc.osgi.bundle.utils.interf.conf.exception.FieldTrackingAssignementException;
 import org.tc.osgi.bundle.utils.interf.exception.TcOsgiException;
 import org.tc.osgi.bundle.utils.interf.module.service.ILoggerUtilsService;
 import org.tc.osgi.bundle.utils.interf.module.service.IPropertyUtilsService;
 import org.tc.osgi.bundle.utils.interf.module.utils.AbstractTcOsgiActivator;
 import org.tc.osgi.bundle.utils.interf.module.utils.TcOsgiProxy;
 
-import spark.Spark;
-
 /**
  * Activator.java.
+ * 
  * @author Collonville Thomas
  * @version 0.0.1
  */
@@ -25,15 +19,6 @@ public class SparkActivator extends AbstractTcOsgiActivator {
 
 	private TcOsgiProxy<ILoggerUtilsService> iLoggerUtilsService;
 	private TcOsgiProxy<IPropertyUtilsService> iPropertyUtilsService;
-	private String restPort;
-	
-	public String getRestPort() throws FieldTrackingAssignementException {
-		if (restPort == null) {
-			this.iPropertyUtilsService.getInstance().getXMLPropertyFile(SparkPropertyFile.getInstance().getXMLFile())
-					.fieldTraking(this, "restPort");
-		}
-		return restPort;
-	}
 
 	@Override
 	protected void checkInitBundleUtilsService(BundleContext context) throws TcOsgiException {
@@ -53,7 +38,6 @@ public class SparkActivator extends AbstractTcOsgiActivator {
 	@Override
 	protected void initServices(BundleContext context) throws TcOsgiException {
 
-		
 	}
 
 	@Override
@@ -65,7 +49,6 @@ public class SparkActivator extends AbstractTcOsgiActivator {
 
 	@Override
 	protected void detachServices(BundleContext context) throws TcOsgiException {
-		this.getIBundleUtilsService().getInstance().unregister(IGroovyService.class, this);
 
 	}
 
@@ -82,13 +65,7 @@ public class SparkActivator extends AbstractTcOsgiActivator {
 
 	@Override
 	protected void afterStart(BundleContext context) throws TcOsgiException {
-		
-		// peut etre que l'on peut pas faire comme ca... 
-		// Spark.port(Integer.parseInt(this.getRestPort()));
-		// il faut peut etre donner la capacité a en gerer plusieur instance de SPark.... donc iici ca serai celui par defaut?
-		// peut on meme en faire un multi instance?
-		// probablement qu'il faudra by passer la classe static
-		//Spark.staticFiles.externalLocation(SparkPropertyFile.getInstance().getWorkDirectory());
+
 	}
 
 	@Override
